5911b05062b107f23d28a2adc65de2b410ab7087
francesco <francesco.peluso@leonardocompany.com>
2023-07-06 12:14:42 +0200

    V1.0

---
+++ ModuleA/Scripts/modA_script1.m
@@ -0,0 +1,3 @@
+in1=1;
+in2=2;
+[out1,out2] = modA_func1(in1,in2);
---
+++ ModuleB/Functions/modB_func2.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = modB_func2(inputArg1,inputArg2)
+%MODB_FUNC2 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
